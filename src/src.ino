b799e81f486395894eb72c4b4f3b724aae67ba60
LeoDJ <leandro.spaeth@gmail.com>
2018-12-23 17:53:46 +0100

    implement dialing via rotary dial

--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,20 @@
+/** ToDo:
+ *    sleep mode (only sleep when handset is down)
+ *    start call when no number input happens for a time
+ */
+
 #include <Arduino.h>
 
-#define interruptPin 2
-#define buttonPressTime 100                     // time in ms to keep the phone button pressed
-uint64_t lastDebounceTime = 0, lastTriggerTime; // the last time the output pin was toggled
-uint64_t debounceDelay = 80;
+#define rotaryDialPin 2        // has to be an interrupt pin
+#define cradlePin 3            // signal of the phone cradle, has to be an interrupt pin
+#define cradleDebounceDelay 50 // debounce time for the cradle switch
+#define buttonPressTime 100    // time in ms to keep the phone button pressed
+uint32_t lastDialPulse = 0;    // the last time a rotary dial pulse has been detected
+#define dialDebounceDelay 80   // debounce time for the rotary dial pulses
 volatile int c = 0;
+
+// command chain for connection to a base station
+char* dectLogonCommand = "XXXXX!^^!vvv!vv!!0000!"; 
 
 byte colPins[] = {4, 5, 6, 7, 8};
 byte rowPins[] = {9, 10, 11, 12};
@@ -41,17 +51,13 @@ enum button {
 
 button buttonMapping[rowPinsLength][colPinsLength] = {{}};
 
-void irq() {
+// interrupt each time the rotary dial generates a pulse
+void dialIRQ() {
     noInterrupts();
-    /*Serial.print(millis() % 1000);
-    Serial.print(" ");
-    Serial.print(digitalRead(interruptPin));*/
-    if (digitalRead(interruptPin) && lastDebounceTime + debounceDelay < millis()) {
-        // Serial.print(" x");
+    if (digitalRead(rotaryDialPin) && lastDialPulse + dialDebounceDelay < millis()) {
         c++;
-        lastDebounceTime = millis();
+        lastDialPulse = millis();
     }
-    // Serial.println();
     interrupts();
 }
 
@@ -66,12 +72,13 @@ void clearButtons() {
 
 void setup() {
     Serial.begin(115200);
-    Serial.println(F(">> Drehscheibentelefon to DECT <<<"));
+    Serial.println(F("\n>> Drehscheibentelefon to DECT <<<"));
     Serial.println(F("\nYou can use ASCII characters to control the phone, apart from dialing with the rotary dial."));
-    Serial.println("\n\n> ");
+    Serial.print("\n\n> ");
 
-    pinMode(interruptPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(interruptPin), irq, CHANGE);
+    pinMode(cradlePin, INPUT_PULLUP);
+    pinMode(rotaryDialPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(rotaryDialPin), dialIRQ, CHANGE);
 
     for (byte i = 0; i < rowPinsLength; i++) {
         pinMode(rowPins[i], INPUT);
@@ -88,23 +95,48 @@ bool buttonPressed = false;
 char scheduledButtons[50];
 bool buttonSchedulingInProgress = false;
 uint8_t scheduledButtonCount = 0, scheduledButtonIdx = 0;
+bool lastCradleState = false;
+uint32_t lastCradleChange = 0;
 
 void loop() {
-    unsigned long diff = millis() - lastDebounceTime;
+    // handle the end of a dial rotation
+    unsigned long diff = millis() - lastDialPulse;
     if (c > 0 && diff > 200 && diff < 500) // catch interrupt modified variable (underflow)
     {
-        // Serial.println(String(millis()) + " " + String(lastDebounceTime) + " " + String(diff) + " " +  String(c));
-        Serial.print(c % 10);
+        // handle the 0 on the rotary dial, as it is located at the end of the wheel
+        if (c == 10) {
+            c = 0;
+        }
+        Serial.print(c);                // repeat the dialed digit to the Serial terminal
+        pressButton((button)(c + '0')); // convert the detected number into an ASCII char
         c = 0;
+    }
+
+    // handle the cradle switch
+    bool cradleState = digitalRead(cradlePin);
+    if (lastCradleState != cradleState && lastCradleChange + cradleDebounceDelay < millis()) {
+        lastCradleChange = millis();
+
+        if (cradleState) { // handset has been put down
+            Serial.print((char)B_CANCEL);
+            pressButton(B_CANCEL);
+        } else { // handset has been lifted up
+            // deactivate sleep count-down
+        }
+
+        lastCradleState = cradleState;
     }
 
     // read stringed buttons to be pressed from serial into buffer
     while (!buttonSchedulingInProgress && Serial.available()) {
         char c = Serial.read();
+
         if (c != '\n') {
+            // put all received chars in buffer
             scheduledButtons[scheduledButtonCount] = c;
             scheduledButtonCount++;
         } else {
+            // upon receiving a newline, trigger the button pressing
             buttonSchedulingInProgress = true;
         }
         Serial.print(c);
@@ -121,6 +153,8 @@ void loop() {
                     buttonSchedulingInProgress = false;
                     Serial.print("\n> ");
                 }
+            } else {
+                buttonSchedulingInProgress = false;
             }
         }
     }
